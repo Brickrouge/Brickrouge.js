923d087ebfee2b631435504ce072c35351511bc7
Olivier Laviale <olivier.laviale@gmail.com>
2016-04-10 11:32:23 +0200

    Using olvlvl-subject

--- Makefile
+++ Makefile
@@ -1,26 +1,28 @@
 BRICKROUGE_JS_COMPRESSED = dist/brickrouge.js
 BRICKROUGE_JS_UNCOMPRESSED = dist/brickrouge-uncompressed.js
-BRICKROUGE_JS_FILES = \
-	lib/core.js \
-	lib/utils.js \
-	lib/subject.js \
-	lib/widget.js
+BRICKROUGE_JS_FILES = $(shell ls lib/*.js)
 
 JS_COMPRESSOR = curl -s \
 	-d compilation_level=SIMPLE_OPTIMIZATIONS \
 	-d output_format=text \
 	-d output_info=compiled_code \
+	-d language=ECMASCRIPT5 \
 	--data-urlencode "js_code@$^" \
 	http://closure-compiler.appspot.com/compile
 #JS_COMPRESSOR = cat $^ # uncomment this line to produce uncompressed files
 
 all: \
+	node_modules \
 	$(BRICKROUGE_JS_UNCOMPRESSED) \
 	$(BRICKROUGE_JS_COMPRESSED)
 
+node_modules:
+	npm install
+
 clean:
-	rm $(BRICKROUGE_JS_COMPRESSED)
-	rm $(BRICKROUGE_JS_UNCOMPRESSED)
+	rm  -f $(BRICKROUGE_JS_COMPRESSED)
+	rm  -f $(BRICKROUGE_JS_UNCOMPRESSED)
+	rm -Rf node_modules
 
 watch:
 	webpack --progress --colors --watch
@@ -30,3 +32,5 @@ $(BRICKROUGE_JS_UNCOMPRESSED): $(BRICKROUGE_JS_FILES)
 
 $(BRICKROUGE_JS_COMPRESSED): $(BRICKROUGE_JS_UNCOMPRESSED)
 	$(JS_COMPRESSOR) > $@
+
+.PHONY: all clean watch
--- dist/brickrouge-uncompressed.js
+++ dist/brickrouge-uncompressed.js
@@ -58,13 +58,14 @@
 		__webpack_require__(4),
 		__webpack_require__(5)
 
-	], __WEBPACK_AMD_DEFINE_RESULT__ = function(Brickrouge, Utils, Subject) {
+	], __WEBPACK_AMD_DEFINE_RESULT__ = function(Brickrouge, Subject) {
 
 		Object.assign(Brickrouge, Subject.prototype)
+		Object.defineProperty(Brickrouge, 'Subject', { value: Subject })
 
 		return window.Brickrouge = Brickrouge
 
-	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__))
 
 
 /***/ },
@@ -80,6 +81,220 @@
 
 /***/ },
 /* 2 */
+/***/ function(module, exports) {
+
+	"use strict";
+
+	const OBSERVERS_PROPERTY = Symbol("Subject observers")
+	const NAME_PROPERTY = Symbol("Subject event name")
+
+	/**
+	 * Asserts that an event name is valid.
+	 *
+	 * @param name
+	 *
+	 * @throws Error if `name` is not a string.
+	 */
+	function assertNameIsValid(name)
+	{
+	    if ('symbol' !== typeof name)
+	    {
+	        throw new Error("Event name is not a symbol")
+	    }
+	}
+
+	/**
+	 * Retrieve event name from an event constructor.
+	 *
+	 * @param {function} constructor
+	 *
+	 * @returns {string}
+	 *
+	 * @throws Error if `constructor` is not an event constructor.
+	 */
+	function retrieveNameFromConstructor(constructor)
+	{
+	    if ('function' !== typeof constructor  || !(NAME_PROPERTY in constructor))
+	    {
+	        throw new Error(`Expecting an event instance, got: ${constructor}`)
+	    }
+
+	    const name = constructor[NAME_PROPERTY]
+
+	    assertNameIsValid(name)
+
+	    return name
+	}
+
+	/**
+	 * Retrieve event name from ab event instance.
+	 *
+	 * @param {object} event
+	 *
+	 * @returns {string}
+	 *
+	 * @throws Error if `event` is not an event instance.
+	 */
+	function retrieveNameFromInstance(event)
+	{
+	    if ('object' !== typeof event || !(NAME_PROPERTY in event.__proto__.constructor))
+	    {
+	        throw new Error("Expected an Event instance")
+	    }
+
+	    const name = event.__proto__.constructor[NAME_PROPERTY]
+
+	    assertNameIsValid(name)
+
+	    return name
+	}
+
+	/**
+	 * Creates an event constructor given a name and a constructor.
+	 *
+	 * @param {function} constructor
+	 *
+	 * @returns {function}
+	 */
+	function createEvent(constructor)
+	{
+	    constructor[NAME_PROPERTY] = Symbol("Event symbol")
+
+	    return constructor
+	}
+
+	/**
+	 * Return the observers array.
+	 *
+	 * @protected
+	 *
+	 * @param {Subject} subject
+	 * @param {string|null} name Event name, or `null` to get all observers.
+	 *
+	 * @return {Array}
+	 */
+	function getObservers(subject, name) {
+
+	    if (!(OBSERVERS_PROPERTY in subject))
+	    {
+	        subject[OBSERVERS_PROPERTY] = []
+	    }
+
+	    const observers = subject[OBSERVERS_PROPERTY]
+
+	    if (!name)
+	    {
+	        return observers
+	    }
+
+	    if (!(name in observers))
+	    {
+	        observers[name] = []
+	    }
+
+	    return observers[name]
+	}
+
+	/**
+	 * @constructor
+	 */
+	function Subject()
+	{
+
+	}
+
+	Subject.prototype = {
+
+	    /**
+	     * Attach an observer.
+	     *
+	     * @param {function} constructor Event constructor.
+	     * @param {function} callback
+	     *
+	     * @return {Subject}
+	     */
+	    observe: function (constructor, callback) {
+
+	        const symbol = retrieveNameFromConstructor(constructor)
+	        const observers = getObservers(this, symbol)
+
+	        if (observers.indexOf(callback) !== -1)
+	        {
+	            throw new Error("Observer already attached", constructor)
+	        }
+
+	        observers.push(callback)
+
+	        return this
+	    },
+
+	    /**
+	     * Detach an observer.
+	     *
+	     * @param {function} callback
+	     *
+	     * @return {Subject}
+	     */
+	    unobserve: function (callback) {
+
+	        const observers = getObservers(this, null)
+
+	        for (let type of Object.getOwnPropertySymbols(observers))
+	        {
+	            let typeObservers = observers[type]
+	            let k = typeObservers.indexOf(callback)
+
+	            if (k === -1)
+	            {
+	                continue
+	            }
+
+	            typeObservers.splice(k, 1)
+	        }
+
+	        return this
+	    },
+
+	    /**
+	     * Notify observers of a change.
+	     *
+	     * @param {object} event
+	     *
+	     * @return {Subject}
+	     */
+	    notify: function (event) {
+
+	        const name = retrieveNameFromInstance(event)
+	        const observers = getObservers(this, name)
+
+	        for (let observer of observers)
+	        {
+	            try
+	            {
+	                observer.call(null, event)
+	            }
+	            catch (e)
+	            {
+	                console.error(e)
+	            }
+	        }
+
+	        return this
+	    }
+	}
+
+	Object.defineProperty(Subject, 'createEvent', { value: createEvent })
+
+	var module
+
+	if (module)
+	{
+	    module.exports = Subject
+	}
+
+
+/***/ },
+/* 3 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
@@ -169,220 +384,13 @@
 
 
 /***/ },
-/* 3 */
-/***/ function(module, exports, __webpack_require__) {
-
-	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
-
-		__webpack_require__(1)
-
-	], __WEBPACK_AMD_DEFINE_RESULT__ = function(Brickrouge) {
-
-		const OBSERVER_PROPERTY = '$brickrouge:observers'
-
-		function assertNameIsValid(name)
-		{
-			if (typeof name !== 'string')
-			{
-				throw new Error("Event name is not a string")
-			}
-		}
-
-		/**
-		 * Retrieve event name from an event constructor.
-		 *
-		 * @param {function} constructor
-		 *
-		 * @returns {string}
-		 */
-		function retrieveNameFromConstructor(constructor)
-		{
-			if (typeof constructor !== 'function' || !('name' in constructor))
-			{
-				throw new Error("Expecting an event instance, got: `" + constructor + "`")
-			}
-
-			let name = constructor.name
-
-			assertNameIsValid(name)
-
-			return name
-		}
-
-		/**
-		 * Retrieve event name from ab event instance.
-		 *
-		 * @param {object} event
-		 *
-		 * @returns {string}
-		 */
-		function retrieveNameFromInstance(event)
-		{
-			if (typeof event !== 'object' || !('name' in event.__proto__.constructor))
-			{
-				throw new Error("Expected an Event instance")
-			}
-
-			let name = event.__proto__.constructor.name
-
-			assertNameIsValid(name)
-
-			return name
-		}
-
-		/**
-		 * @interface
-		 */
-		function Subject()
-		{
-
-		}
-
-		Subject.prototype = {
-
-			/**
-			 * Return the observers array.
-			 *
-			 * @protected
-			 *
-			 * @param {string|null} name Event name, or `null` to get all observers.
-			 *
-			 * @return {Array}
-			 */
-			getObservers: function (name) {
-
-				let observers
-
-				if (!(OBSERVER_PROPERTY in this)) {
-					this[OBSERVER_PROPERTY] = []
-				}
-
-				observers = this[OBSERVER_PROPERTY]
-
-				if (!name) {
-					return observers
-				}
-
-				if (!(name in observers)) {
-					observers[name] = []
-				}
-
-				return observers[name]
-			},
-
-			/**
-			 * Attach an observer.
-			 *
-			 * @param {function} constructor Event constructor.
-			 * @param {function} callback
-			 *
-			 * @return {this}
-			 */
-			observe: function (constructor, callback) {
-
-				let name = retrieveNameFromConstructor(constructor)
-				let observers = this.getObservers(name)
-
-				if (observers.indexOf(callback) !== -1)
-				{
-					throw new Error("Observer already attached for event `" + name + '`')
-				}
-
-				observers.push(callback)
-
-				return this
-			},
-
-			/**
-			 * Detach an observer.
-			 *
-			 * @param {function} callback
-			 *
-			 * @return {this}
-			 */
-			unobserve: function (callback) {
-
-				let observers = this.getObservers(null)
-				let typeObservers
-
-				for (let type in Object.keys(observers))
-				{
-					typeObservers = observers[type]
-					let k = typeObservers.indexOf(callback)
-
-					if (k === -1) continue
-
-					typeObservers.splice(k, 1)
-				}
-
-				return this
-			},
-
-			/**
-			 * Notify observers of a change.
-			 *
-			 * @param {object} event
-			 *
-			 * @return {this}
-			 */
-			notify: function (event) {
-
-				let name = retrieveNameFromInstance(event)
-				let observers = this.getObservers(name)
-
-				if (!observers.length)
-				{
-					return this
-				}
-
-				for (let i = 0, y = observers.length; i < y; i++)
-				{
-					try
-					{
-						observers[i].call(null, event)
-					}
-					catch (e)
-					{
-						console.error(e)
-					}
-				}
-
-				return this
-			}
-		}
-
-		/**
-		 * Creates an event given a name and a constructor.
-		 *
-		 * @param {string} name
-		 * @param {function} constructor
-		 *
-		 * @returns {function}
-		 */
-		Subject.createEvent = function (name, constructor) {
-
-			Object.defineProperty(constructor, 'name', { value: name })
-
-			return constructor
-
-		}
-
-		/**
-		 * @interface
-		 */
-		return Brickrouge.Subject = Subject
-
-	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__))
-
-
-/***/ },
 /* 4 */
 /***/ function(module, exports, __webpack_require__) {
 
 	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;!(__WEBPACK_AMD_DEFINE_ARRAY__ = [
 
 		__webpack_require__(1),
-		__webpack_require__(3)
+		__webpack_require__(2)
 
 	], __WEBPACK_AMD_DEFINE_RESULT__ = function(Brickrouge, Subject) {
 
@@ -394,15 +402,15 @@
 		const OPTIONS_ATTRIBUTE = 'brickrouge-options'
 		const WIDGET_SELECTOR = '[' + IS_ATTRIBUTE + ']'
 
-		var factories = []
-		var widgets = []
-		var parsed = []
+		const factories = []
+		const widgets = []
+		const parsed = []
 
 		/**
 		 * @event Brickrouge#running
 		 * @type {object}
 		 */
-		let RunningEvent = Subject.createEvent('running', function () {
+		const RunningEvent = Subject.createEvent(function () {
 
 		})
 
@@ -413,7 +421,7 @@
 		 * @type {object}
 		 * @property {object} widget - The widget that was built.
 		 */
-		let WidgetEvent = Subject.createEvent('widget', function (widget) {
+		const WidgetEvent = Subject.createEvent(function (widget) {
 
 			this.widget = widget
 
@@ -430,7 +438,7 @@
 		 * @property {Array<Element>} elements - The new widget elements.
 		 * @property {Array<object>} widgets - The widgets that were built.
 		 */
-		let UpdateEvent = Subject.createEvent('update', function (fragment, elements, widgets) {
+		const UpdateEvent = Subject.createEvent(function (fragment, elements, widgets) {
 
 			this.fragment = fragment
 			this.elements = elements
